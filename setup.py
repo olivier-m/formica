a721c554862f18e49ceaa47836f3a55ea34e2163
Olivier Meunier <olivier@neokraft.net>
2014-06-16 16:41:52 +0200

    Fixed setup.py script

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@
 #
 # This file is part of Formica released under the FreeBSD license.
 # See the LICENSE for more information.
-from __future__ import (print_function, division, absolute_import, unicode_literals)
-
 from setuptools import setup
 
 # Get app version
